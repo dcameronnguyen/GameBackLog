eb7b508f2cc5e0b8824f2cbc2178311be3b08319
dcameronnguyen <dcameron.nguyen@gmail.com>
2020-11-17 17:57:49 -0500

    Deploy to Heroku

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const methodOverride = require('method-override');
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const indexRouter = require('./routes/index');
 const loggerRouter = require('./routes/logger');
